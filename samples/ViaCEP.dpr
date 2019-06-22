f4ae834a94c0ec28b44a9229edd132a044b1db0e
viniciussanchez <viniciuss.sanchez@gmail.com>
2019-06-22 11:29:32 -0300

    Moved samples unit for samples folder

--- samples/ViaCEP.dpr
+++ samples/ViaCEP.dpr
@@ -2,10 +2,10 @@ program ViaCEP;
 
 uses
   Vcl.Forms,
-  ViaCEP.Core in 'src\ViaCEP.Core.pas',
-  ViaCEP.Intf in 'src\ViaCEP.Intf.pas',
-  ViaCEP.Model in 'src\ViaCEP.Model.pas',
-  ViaCEP.Samples in 'samples\ViaCEP.Samples.pas' {FrmMain};
+  ViaCEP.Core in '..\src\ViaCEP.Core.pas',
+  ViaCEP.Intf in '..\src\ViaCEP.Intf.pas',
+  ViaCEP.Model in '..\src\ViaCEP.Model.pas',
+  ViaCEP.Samples in 'src\ViaCEP.Samples.pas' {FrmMain};
 
 {$R *.res}
 
